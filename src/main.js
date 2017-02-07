a39ee6434951b3fe8b062d433092f9bf4d9ec527
shelune <hieu@mios.fi>
2017-02-07 22:41:16 +0200

    added vue-resource

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import App from './App'
 
 // OUTER WRAPPER STAGE //
@@ -26,6 +27,7 @@ import SupportsCard from 'components/SupportsCard.vue'
 import SupportsParty from 'components/SupportsParty.vue'
 
 Vue.use(VueRouter)
+Vue.use(VueResource)
 /* eslint-disable no-new */
 
 const router = new VueRouter({
